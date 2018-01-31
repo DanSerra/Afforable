9202ca47f2e876f7547ed1ec72f52cd3285a203f
DanSerra <serrano.r.daniela@gmail.com>
2018-01-30 21:19:41 -0600

    final

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ var $pantalla1 = $('#pantalla1');
 var $pantalla2 = $('#pantalla2');
 $pantalla1.fadeOut(5000);
 $pantalla2.fadeIn(5000);
-
 //login
 var provider = new firebase.auth.GoogleAuthProvider();//Instancia del provedor de servicio
 $('#login').click(function(){
@@ -14,7 +13,6 @@ $('#login').click(function(){
     guardaDatos(result.user);
     $('#login').hide();
     $('#root').append("<img src='"+result.user.photoURL+"' />");
-
   });
 });
 //Guarda automáticamente
@@ -28,7 +26,6 @@ function guardaDatos(user) {
   firebase.database().ref("Prueba1/" +user.uid)
   .set(usuario)
 }
-
 //Guardar en Base de datos información ingresada como objeto
 $('#guardar').click(function(){
   firebase.database().ref("redSocial")
@@ -38,40 +35,31 @@ $('#guardar').click(function(){
     sexo:"femenino"
   })
 });
-
 //Leer base de guardaDatos
 firebase.database().ref("Prueba1")
 .on("child_added", function(s){
   var user= s.val();
   $('#root').append("<img width='100px' src='"+user.foto+"' />");
-
 })
-
 //Seleccionar los elementos del html para manipularlos con js
 var addTweet = document.forms['add-tweet'];
 var textArea = document.getElementById('tweet');
 var timeLine = document.getElementById('time-line');
 var counterNumber = document.getElementById('character-count')
 var button = document.getElementById('btn')
-
 //Evento para identificar el click en el formulario del tweet
 addTweet.addEventListener('submit', printTweet);
-
 //Evento para dentificar el número de veces que se presiona una tecla
 textArea.addEventListener('keyup', counter)
-
 //Evento para identificar el click
 button.addEventListener('click', clearCounter)
-
 //Evento que identifica cambios en la textarea
 textArea.addEventListener('input', resizeTextarea);
-
 //Función que resetea el contador cada vez que se publica un tweet
 function clearCounter() {
     counterNumber.textContent = 140;
-    counterNumber.style.color = 'black';
+    counterNumber.style.color = '#fff';
 }
-
 //Función que se desencadena al picarle al botón del formulario que crea los elementos para el nuevo tweet, asigna el contenido del textarea e imprime fecha
 function printTweet(e) {
     e.preventDefault();
@@ -86,9 +74,9 @@ function printTweet(e) {
     timeLine.prepend(newTweet);
     textArea.value = ''; //preguntar
     button.setAttribute('disabled', 'true');
-    button.style.backgroundColor = '#ededed';
+    button.style.backgroundColor = '#6E1FFF';
+    button.style.color = '#FFF';
 };
-
 //Botón desabilitado (se asignó ese atributo en HTML y por eso se establece su color como gris)
 button.style.backgroundColor = '#ededed';
 //Función para contar caracteres y cambiar el color según el número de caracteres, habilitar el botón si se introducen caracteres a la textarea.
@@ -96,22 +84,21 @@ function counter() {
     var characters = textArea.value.split('');
     counterNumber.innerText = 140 - (characters.length);
     if (characters.length >= 120 && characters.length < 130) {
-        counterNumber.style.color = '#c4ba48';
+        counterNumber.style.color = '#fff';
     } else if (characters.length >= 130 && characters.length < 140) {
         counterNumber.style.color = 'red';
     } else if (characters.length < 120) {
-        counterNumber.style.color = 'black';
+        counterNumber.style.color = 'fff';
     }
     if (characters.length > 0) {
         button.removeAttribute('disabled')
-        button.style.backgroundColor = '#61D3F0';
+        button.style.backgroundColor = '#6E1FFF';
     }
     if (textArea.value == '' || characters.length > 140 || textArea.value.trim().length === 0) {
         button.setAttribute('disabled', 'true');
         button.style.backgroundColor = '#ededed';
     }
 }
-
 //Función para obtener la hora con formato hh:mm
 function getDate() {
     var time = new Date();
@@ -129,11 +116,8 @@ function getDate() {
     }
     return hours + ':' + minutes + ' ' + meridian;
 }
-
 //Función de autoajuste para la textarea
 function resizeTextarea() {
-
     this.style.height = '24px';
     this.style.height = this.scrollHeight + 12 + 'px'
-
 }
